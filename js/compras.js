b3605938118ceafa2442c45a901711d59eb6af9b
AndyR <marlon_rivera95@hotmail.com>
2022-04-14 13:35:31 -0500

    primera subida

---
+++ js/compras.js
@@ -0,0 +1,142 @@
+document.querySelector("#submit1").addEventListener("click", e => {
+    e.preventDefault();
+
+    //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+    let telefono = "573172171691";
+
+    let ref1 = document.querySelector("#ref1").textContent;
+    let r1 = document.querySelector("#r1").textContent;
+    let precio1 = document.querySelector("#precio1").textContent;
+
+    let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+        *_Nombre empresa_*%0A
+        *Tu compra*%0A%0A
+        *Ref del producto:*%0D
+        ${ref1}%0A%0A
+        *Nombre del producto:*%0D
+        ${r1}%0A%0A
+        *Precio:*%0D
+        ${precio1}%0A%0A`;
+
+    window.open(url);     
+});
+
+
+document.querySelector("#submit2").addEventListener("click", e => {
+    e.preventDefault();
+
+    //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+    let telefono = "573172171691";
+
+    let ref2 = document.querySelector("#ref2").textContent;
+    let r2 = document.querySelector("#r2").textContent;
+    let precio2 = document.querySelector("#precio2").textContent;
+
+    let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+		*_Nombre empresa_*%0A
+		*Tu compra*%0A%0A
+		*Ref del producto:*%0D
+		${ref2}%0A%0A
+		*Nombre del producto:*%0D
+		${r2}%0A%0A
+		*Precio:*%0D
+		${precio2}%0A%0A`;
+
+    window.open(url);    
+});
+
+
+document.querySelector("#submit3").addEventListener("click", e => {
+    e.preventDefault();
+
+    //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+    let telefono = "573172171691";
+
+    let ref3 = document.querySelector("#ref3").textContent;
+    let r3 = document.querySelector("#r3").textContent;
+    let precio3 = document.querySelector("#precio3").textContent;
+
+    let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+		*_Nombre empresa_*%0A
+		*Tu compra*%0A%0A
+		*Ref del producto:*%0D
+		${ref3}%0A%0A
+		*Nombre del producto:*%0D
+		${r3}%0A%0A
+		*Precio:*%0D
+		${precio3}%0A%0A`;
+
+    window.open(url);    
+});
+
+
+document.querySelector("#submit4").addEventListener("click", e => {
+    e.preventDefault();
+
+    //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+    let telefono = "573172171691";
+
+    let ref4 = document.querySelector("#ref4").textContent;
+    let r4 = document.querySelector("#r4").textContent;
+    let precio4 = document.querySelector("#precio4").textContent;
+
+    let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+		*_Nombre empresa_*%0A
+		*Tu compra*%0A%0A
+		*Ref del producto:*%0D
+		${ref4}%0A%0A
+		*Nombre del producto:*%0D
+		${r4}%0A%0A
+		*Precio:*%0D
+		${precio4}%0A%0A`;
+
+    window.open(url);    
+});
+
+
+document.querySelector("#submit5").addEventListener("click", e => {
+    e.preventDefault();
+
+    //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+    let telefono = "573172171691";
+
+    let ref5 = document.querySelector("#ref5").textContent;
+    let r5 = document.querySelector("#r5").textContent;
+    let precio5 = document.querySelector("#precio5").textContent;
+
+    let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+		*_Nombre empresa_*%0A
+		*Tu compra*%0A%0A
+		*Ref del producto:*%0D
+		${ref5}%0A%0A
+		*Nombre del producto:*%0D
+		${r5}%0A%0A
+		*Precio:*%0D
+		${precio5}%0A%0A`;
+
+    window.open(url);    
+});
+
+
+document.querySelector("#submit6").addEventListener("click", e => {
+    e.preventDefault();
+
+    //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
+    let telefono = "573172171691";
+
+    let ref6 = document.querySelector("#ref6").textContent;
+    let r6 = document.querySelector("#r6").textContent;
+    let precio6 = document.querySelector("#precio6").textContent;
+
+    let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
+		*_Nombre empresa_*%0A
+		*Tu compra*%0A%0A
+		*Ref del producto:*%0D
+		${ref6}%0A%0A
+		*Nombre del producto:*%0D
+		${r6}%0A%0A
+		*Precio:*%0D
+		${precio6}%0A%0A`;
+
+    window.open(url);    
+});
---
+++ js/script.js
@@ -0,0 +1,30 @@
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times'); /* activa la x */
+    navbar.classList.toggle('active');
+}
+
+/* desaparece el menú responsivo al darle click en un link */
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    effect: "fade",
+    grabCursor:true,
+    loop:true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    centeredSlides:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
